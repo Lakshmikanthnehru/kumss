4176fde4a7fc020b2e58986a12f141463aad2a7b
Lakshmikanthnehru <luxsachin10@gmail.com>
2022-10-03 11:35:26 +0530

    mm

--- src/org/test/GitBash.java
+++ src/org/test/GitBash.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class GitBash {
+	private void employeename() {
+	System.out.println("employee name is arun");
 
+	}
+
+	private void clientid() {
+		System.out.println("client id is z23100");
+
+	}
 }
